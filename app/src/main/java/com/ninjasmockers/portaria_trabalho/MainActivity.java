fd5c6f9436673de16950a472ebac18533b537b5f
ARIONILDO MENDES ESCOUTO <ALU201523294@uniritter.local>
2019-04-03 21:38:30 -0300

    Adição Permissão Calendário

--- app/src/main/java/com/ninjasmockers/portaria_trabalho/MainActivity.java
+++ app/src/main/java/com/ninjasmockers/portaria_trabalho/MainActivity.java
@@ -1,11 +1,15 @@
 package com.ninjasmockers.portaria_trabalho;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this,CadastroLogin.class));
     }
 
+    protected void login(View view) {
+        Intent intent = new Intent();
+        ComponentName componentName = new ComponentName("com.google.android.calendar", "com.android.calendar.LaunchActivity");
+        intent.setComponent(componentName);
+        startActivity(intent);
+    }
 
 }
