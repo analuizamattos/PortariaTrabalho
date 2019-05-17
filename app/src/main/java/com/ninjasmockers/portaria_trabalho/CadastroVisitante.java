56b76616a187e15bc4406b18bb686ff3b91e8ade
Douglas Angelo <douglasdcangelo@gmail.com>
2019-05-17 11:54:57 -0300

    Adicionado mais duas telas

---
+++ app/src/main/java/com/ninjasmockers/portaria_trabalho/CadastroVisitante.java
@@ -0,0 +1,13 @@
+package com.ninjasmockers.portaria_trabalho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroVisitante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_visitante);
+    }
+}
