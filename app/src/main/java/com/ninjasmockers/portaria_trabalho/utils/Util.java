9d662c192766b8766039599100f8e14547131b62
DOUGLAS CREMONINI ANGELO <ALU201723153@uniritter.local>
2019-06-05 21:05:21 -0300

    Ingles e mais

--- app/src/main/java/com/ninjasmockers/portaria_trabalho/utils/Util.java
+++ app/src/main/java/com/ninjasmockers/portaria_trabalho/utils/Util.java
@@ -3,8 +3,13 @@ package com.ninjasmockers.portaria_trabalho.utils;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Util {
@@ -25,20 +30,23 @@ public class Util {
 
     public static String buscarPiada() throws IOException {
         String endereco = "https://api.chucknorris.io/jokes/random";
-
         URL url = new URL(endereco);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.connect();
+        InputStream stream = connection.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 
         StringBuffer buffer = new StringBuffer();
-        String line = "";
+        String line;
 
         while ((line = reader.readLine()) != null) {
             buffer.append(line);
         }
-        
-        JSONObject mainObject = new JSONObject(buffer.toString());
-        return mainObject.getJsonString("value");
+        try {
+            JSONObject mainObject = new JSONObject(buffer.toString());
+            return mainObject.getString("value");
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
