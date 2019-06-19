adf9085beeb0c38d04c3f4a5954ce6238471aa95
ArionildoEscouto <arionildo6@gmail.com>
2019-06-18 22:07:09 -0300

    Add Drag and Drop

--- app/src/main/java/com/ninjasmockers/portaria_trabalho/activities/Home.java
+++ app/src/main/java/com/ninjasmockers/portaria_trabalho/activities/Home.java
@@ -1,17 +1,26 @@
 package com.ninjasmockers.portaria_trabalho.activities;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.ninjasmockers.portaria_trabalho.R;
-import com.ninjasmockers.portaria_trabalho.entity.Usuario;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnDragListener, View.OnLongClickListener {
     private FirebaseAuth autenticacao;
 
    // private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
@@ -23,6 +32,14 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Button btn = findViewById(R.id.dragButton);
+        btn.setTag("DRAGGABLE BUTTON");
+        btn.setOnLongClickListener(this);
+        findViewById(R.id.recyclerView).setOnDragListener(this);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        ((RecyclerView)findViewById(R.id.recyclerView)).setLayoutManager(manager);
         /*referencia.child("pontos").setValue("100");
         referencia.child("produtos").child("001").child("descricao").setValue("IfoneX");
 
@@ -42,5 +59,68 @@ public class Home extends AppCompatActivity {
     protected void sair(View view) {
         startActivity(new Intent(this, MainActivity.class));
         //autenticacao.signOut();
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+        View.DragShadowBuilder dragShadow = new View.DragShadowBuilder(v);
+        v.startDrag(data, dragShadow, v, 0);
+        return true;
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        int action = event.getAction();
+        switch (action) {
+
+            case DragEvent.ACTION_DRAG_STARTED:
+                return event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN);
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                v.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                v.getBackground().clearColorFilter();
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                ClipData.Item item = event.getClipData().getItemAt(0);
+                String dragData = item.getText().toString();
+                Toast.makeText(this, "Objeto pego é " + dragData, Toast.LENGTH_SHORT).show();
+                v.getBackground().clearColorFilter();
+                v.invalidate();
+
+                View vw = (View) event.getLocalState();
+                ViewGroup container = (RecyclerView) v;
+
+                if(vw.getParent() != null) {
+                    ((ViewGroup)vw.getParent()).removeView(vw);
+                }
+                container.addView(vw);
+                vw.setVisibility(View.VISIBLE);
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                v.getBackground().clearColorFilter();
+                v.invalidate();
+                if (event.getResult())
+                    Toast.makeText(this, "Objeto reposicionado com sucesso.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "Falha ao soltar.", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                Log.e("DragDrop", "Ação desconhecida.");
+                break;
+        }
+        return false;
     }
 }
